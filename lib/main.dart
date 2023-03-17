9d77ed054b050bbe8b63c11ca83b758860a7d979
Wagner_Silva <wagner_silva2332@hotmail.com>
2023-03-17 02:27:45 +0000

    home-page

--- lib/main.dart
+++ lib/main.dart
@@ -1,171 +1,32 @@
 import 'package:flutter/material.dart';
-import 'item.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const SignUpApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SignUpApp extends StatelessWidget {
+  const SignUpApp();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+      routes: {
+        '/': (context) => const SignUpScreen(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter UI Layouts Home Page'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.grid_on),
-              ),
-              Tab(
-                icon: Icon(Icons.list),
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            // GridView tab content Widget
-            GridView.count(
-              // Items in row
-              crossAxisCount: 2,
-              // Vertical spacing between rows
-              mainAxisSpacing: 5.0,
-              // Horizontal spacing between columns
-              crossAxisSpacing: 5.0,
-              // Padding of GridView
-              padding: const EdgeInsets.all(5.0),
-              // The ratio between the width and height of items
-              childAspectRatio: 0.75,
-              // List of items widgets
-              children: items.map<Widget>((Item item) => _ItemGridCellWidget(item)).toList(),
-            ),
-            // ListView tab content Widget
-            ListView.builder(itemCount: items.length, itemBuilder: (BuildContext context, int position) => _ItemListCellWidget(items[position]))
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _ItemGridCellWidget extends StatelessWidget {
-  final Item _item;
-
-  _ItemGridCellWidget(this._item);
-
-  void _selectItem(BuildContext context) {
-    Navigator.push(
-        context,
-        MaterialPageRoute<void>(
-          builder: (BuildContext context) => _ItemFullScreenWidget(_item),
-        ));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GridTile(
-        footer: GridTileBar(
-          title: Text(_item.name),
-          subtitle: Text(_item.description),
-          backgroundColor: Colors.black38,
-        ),
-        child: GestureDetector(
-          onTap: () => _selectItem(context),
-          child: Hero(
-            key: Key(_item.imageUrl),
-            tag: _item.name,
-            child: Image.network(
-              _item.imageUrl,
-              fit: BoxFit.cover,
-            ),
-          ),
-        ));
-  }
-}
-
-class _ItemListCellWidget extends StatelessWidget {
-  final Item _item;
-
-  _ItemListCellWidget(this._item);
-
-  void _selectItem(BuildContext context) {
-    Navigator.push(
-        context,
-        MaterialPageRoute<void>(
-          builder: (BuildContext context) => _ItemFullScreenWidget(_item),
-        ));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () => _selectItem(context),
-//      isThreeLine: true,
-      title: Text(
-        _item.name,
-        style: TextStyle(
-          fontWeight: FontWeight.w500,
-          fontSize: 20,
-        ),
-      ),
-      subtitle: Text(
-        _item.description,
-        maxLines: 2,
-        style: TextStyle(),
-      ),
-      leading: Hero(
-        key: Key(_item.imageUrl),
-        tag: _item.name,
-        child: Image.network(
-          _item.imageUrl,
-          width: 50,
-          height: 50,
-          fit: BoxFit.cover,
-        ),
-      ),
-    );
-  }
-}
-
-class _ItemFullScreenWidget extends StatelessWidget {
-  final Item _item;
-
-  _ItemFullScreenWidget(this._item);
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(_item.name),
-      ),
-      body: SizedBox.expand(
-        child: Hero(
-          tag: _item.name,
-          child: Image.network(
-            _item.imageUrl,
-            fit: BoxFit.cover,
+      backgroundColor: Colors.blue[200],
+      body: const Center(
+        child: SizedBox(
+          width: 400,
+          child: Card(
+            child: SignUpForm(),
           ),
         ),
       ),
@@ -173,40 +34,70 @@ class _ItemFullScreenWidget extends StatelessWidget {
   }
 }
 
-List<Item> items = [
-  Item(
-    "https://i.pinimg.com/originals/df/0a/3e/df0a3e2ec30abb1c92d145ef165b714f.gif",
-    "Nature 1",
-    """Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""",
-  ),
-  Item(
-    "https://i.pinimg.com/originals/40/c3/be/40c3bef82a8077e5c872808eefff5c6d.png",
-    "Nature 2",
-    "Description",
-  ),
-  Item(
-    "https://image.freepik.com/free-vector/flat-design-baby-shark-cartoon-style_52683-36255.jpg",
-    "Nature 3",
-    "Description",
-  ),
-  Item(
-    "https://image.freepik.com/free-vector/fairytale-concept-with-child-reading_23-2148472951.jpg",
-    "Sensetpier",
-    "Description",
-  ),
-  Item(
-    "https://img.freepik.com/free-vector/spring-landscape-scene_23-2148860692.jpg",
-    "Nature 5",
-    "Description",
-  ),
-  Item(
-    "https://pw.artfile.me/wallpaper/20-03-2017/650x366/vektornaya-grafika-priroda--nature-sneg--1143282.jpg",
-    "Night Nature",
-    "Description",
-  ),
-];
+class SignUpForm extends StatefulWidget {
+  const SignUpForm();
+
+  @override
+  State<SignUpForm> createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final _firstNameTextController = TextEditingController();
+  final _lastNameTextController = TextEditingController();
+  final _usernameTextController = TextEditingController();
+
+  double _formProgress = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          LinearProgressIndicator(value: _formProgress),
+          Text('Home Page', style: Theme.of(context).textTheme.headlineMedium),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: _firstNameTextController,
+              decoration: const InputDecoration(hintText: 'Nome:'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: _lastNameTextController,
+              decoration: const InputDecoration(hintText: 'E-mail:'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              obscureText: true,
+              controller: _usernameTextController,
+              decoration: const InputDecoration(hintText: 'Senha:'),
+            ),
+          ),
+          TextButton(
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.resolveWith(
+                  (Set<MaterialState> states) {
+                return states.contains(MaterialState.pressed)
+                    ? null
+                    : Colors.black;
+              }),
+              backgroundColor: MaterialStateProperty.resolveWith(
+                  (Set<MaterialState> states) {
+                return states.contains(MaterialState.pressed)
+                    ? null
+                    : Colors.blue;
+              }),
+            ),
+            onPressed: null,
+            child: const Text('Entrar'),
+          ),
+        ],
+      ),
+    );
+  }
+}
